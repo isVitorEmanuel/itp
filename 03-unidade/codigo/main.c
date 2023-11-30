e672636b28ed8e7064d1f862ce1c866123e54826
Vitor Emanuel Rodrigues de Alencar <vitor.alencar.713@ufrn.edu.br>
2023-11-30 14:17:52 -0300

    Implentação das Funções de Manipulação de Arquivos

--- 03-unidade/codigo/main.c
+++ 03-unidade/codigo/main.c
@@ -10,7 +10,6 @@ typedef enum { CHAR, STRING, INT, FLOAT, DOUBLE } Types;
 
 // Definição da estrutura da célula - OK
 typedef struct {
-  Types tipoCelula;
   char dataChar;
   int dataInt;
   float dataFloat;
@@ -88,6 +87,7 @@ Tabela *inserirTabela(Tabela *tabelas, int *numTab, Tabela novaTabela) {
   tabelas = realloc(tabelas, sizeof(Tabela) * (*numTab));
 
   tabelas[*numTab - 1] = novaTabela;
+
   return tabelas;
 }
 // Função que lista as tabelas disponíveis - OK
@@ -97,7 +97,7 @@ void listarTabelas(Tabela *tabelas, int numTab) {
     printf("Nome da tabela: %s\n", tabelas[i].nomeTabela);
   }
 }
-
+// Função que adiciona linha - OK
 void adicionarLinha(Tabela *tabela) {
 
   int chavePrimaria;
@@ -160,13 +160,220 @@ void adicionarLinha(Tabela *tabela) {
     }
   }
 }
-void listarDadosTabela() {}
+
+void salvarTabela(Tabela tabela) {
+
+  char nomeArquivo[MAX_NAME];
+  sprintf(nomeArquivo, "%s.txt", tabela.nomeTabela);
+
+  FILE *arquivo = fopen(nomeArquivo, "w");
+  if (arquivo != NULL) {
+    fprintf(arquivo, "%s\n", tabela.nomeTabela);
+    fprintf(arquivo, "%d %d\n", tabela.numColunas, tabela.numLinhas);
+    for (int i = 0; i < tabela.numColunas; i++) {
+      fprintf(arquivo, "%s %d\n", tabela.colunas[i].nomeColuna,
+              tabela.colunas[i].tipoColuna);
+    }
+    for (int i = 0; i < tabela.numLinhas; i++) {
+      for (int j = 0; j < tabela.numColunas; j++) {
+        switch (tabela.colunas[j].tipoColuna) {
+        case CHAR:
+          fprintf(arquivo, "%c ", tabela.dados[i][j].dataChar);
+          break;
+        case STRING:
+          fprintf(arquivo, "%s ", tabela.dados[i][j].dataString);
+          break;
+        case INT:
+          fprintf(arquivo, "%d ", tabela.dados[i][j].dataInt);
+          break;
+        case FLOAT:
+          fprintf(arquivo, "%f ", tabela.dados[i][j].dataFloat);
+          break;
+        case DOUBLE:
+          fprintf(arquivo, "%lf ", tabela.dados[i][j].dataDouble);
+          break;
+        default:
+          break;
+        }
+      }
+      fprintf(arquivo, "\n");
+    }
+    fclose(arquivo);
+  } else {
+    printf("Error opening file!\n");
+  }
+}
+
+void listarDadosTabela(Tabela tabela) {
+  for (int i = 0; i < tabela.numLinhas; i++) {
+    for (int j = 0; j < tabela.numColunas; j++) {
+      switch (tabela.colunas[j].tipoColuna) {
+      case CHAR:
+        printf("%c", tabela.dados[i][j].dataChar);
+        break;
+      case STRING:
+        printf("%s", tabela.dados[i][j].dataString);
+        break;
+      case INT:
+        printf("%i", tabela.dados[i][j].dataInt);
+        break;
+      case FLOAT:
+        printf("%f", tabela.dados[i][j].dataFloat);
+        break;
+      case DOUBLE:
+        printf("%lf", tabela.dados[i][j].dataDouble);
+        break;
+      default:
+        // Tratamento para tipos desconhecidos
+        break;
+      }
+      printf(" ");
+    }
+    printf("\n");
+  }
+}
 
 void pesquisarValores() {}
 
 void apagarLinha() {}
 
 void apagarTabela() {}
+
+void *carregarTabelas(Tabela *tabelas, int *numTabs) {
+  FILE *arquivo = fopen("tabelas.txt", "r");
+
+  char nomeTabela[MAX_NAME];
+  char nomeArquivoTabela[MAX_NAME + 11];
+
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo de lista de tabelas!\n");
+  }
+
+  while (fgets(nomeTabela, MAX_NAME, arquivo) != NULL) {
+    // Remove o caractere de nova linha, se presente
+    nomeTabela[strcspn(nomeTabela, "\n")] = '\0';
+
+    printf("%s\n", nomeTabela);
+
+    sprintf(nomeArquivoTabela, "%s.txt", nomeTabela);
+    printf("Arquivo da Tabela: %s\n", nomeArquivoTabela);
+
+    FILE *arquivoTabela = fopen(nomeArquivoTabela, "r");
+
+    if (arquivoTabela == NULL) {
+      continue;
+    } else {
+      printf("abriu\n");
+    }
+    // Cria uma nova tabela no vetor
+    Tabela novaTabela;
+    // Nome
+    fscanf(arquivoTabela, "%s", novaTabela.nomeTabela);
+
+    printf("Nome: %s\n", novaTabela.nomeTabela);
+    // Colunas
+    fscanf(arquivoTabela, "%d %d", &novaTabela.numColunas,
+           &novaTabela.numLinhas);
+
+    printf("Colunas: %d\n", novaTabela.numColunas);
+    printf("Linhas: %d\n", novaTabela.numLinhas);
+
+    // Aloca espaço para o vetor colunas
+
+    novaTabela.colunas = calloc(novaTabela.numColunas, sizeof(Coluna));
+
+    // Lê o nome e tipo de cada coluna
+    for (int i = 0; i < novaTabela.numColunas; i++) {
+      fscanf(arquivoTabela, "%s %d", novaTabela.colunas[i].nomeColuna,
+             &novaTabela.colunas[i].tipoColuna);
+    }
+
+    for (int i = 0; i < novaTabela.numColunas; i++) {
+      printf("Nome Coluna: %s - Tipo Coluna: %d\n",
+             novaTabela.colunas[i].nomeColuna,
+             novaTabela.colunas[i].tipoColuna);
+    }
+    // // Aloca espaço para a matriz de células
+    novaTabela.dados = malloc(novaTabela.numLinhas * sizeof(Celula *));
+
+    for (int i = 0; i < novaTabela.numLinhas; i++) {
+      novaTabela.dados[i] = malloc(novaTabela.numColunas * sizeof(Celula));
+    }
+
+    // Lê os dados de cada célula
+    for (int i = 0; i < novaTabela.numLinhas; i++) {
+      for (int j = 0; j < novaTabela.numColunas; j++) {
+        switch (novaTabela.colunas[j].tipoColuna) {
+        case CHAR:
+          fscanf(arquivoTabela, "%c", &novaTabela.dados[i][j].dataChar);
+          break;
+        case STRING:
+          novaTabela.dados[i][j].dataString = malloc(MAX_NAME * sizeof(char));
+          fscanf(arquivoTabela, "%s ", novaTabela.dados[i][j].dataString);
+          break;
+        case INT:
+          fscanf(arquivoTabela, "%d", &novaTabela.dados[i][j].dataInt);
+          break;
+        case FLOAT:
+          fscanf(arquivoTabela, "%f", &novaTabela.dados[i][j].dataFloat);
+          break;
+        case DOUBLE:
+          fscanf(arquivoTabela, "%lf", &novaTabela.dados[i][j].dataDouble);
+          break;
+        default:
+          // Tratamento para tipos desconhecidos
+          break;
+        }
+      }
+    }
+
+    (*numTabs)++;
+    tabelas = realloc(tabelas, sizeof(Tabela) * (*numTabs));
+    tabelas[*numTabs - 1] = novaTabela;
+    printf("NUM TABS: %d\n", *numTabs);
+    printf("SO: %s\n", novaTabela.nomeTabela);
+    fclose(arquivoTabela);
+  }
+
+  printf("NCOISOO: %s", tabelas[*numTabs - 1].nomeTabela);
+
+  fclose(arquivo);
+
+  return tabelas;
+}
+
+void salvarNome(Tabela *tabelas, int numTabs) {
+  FILE *arqTab = fopen("tabelas.txt", "w");
+
+  if (arqTab == NULL) {
+    printf("Arquivo não encontrado!");
+  } else {
+    for (int i = 0; i < numTabs; i++) {
+      fprintf(arqTab, "%s\n", tabelas[i].nomeTabela);
+    }
+  }
+  fclose(arqTab);
+}
+
+void salvarQuantidade(int numTabs) {
+  FILE *arqTab = fopen("qtdtabelas.txt", "w");
+
+  if (arqTab == NULL) {
+    printf("Arquivo não encontrado!");
+  } else {
+    fprintf(arqTab, "%d", numTabs);
+  }
+  fclose(arqTab);
+}
+
+void carregarQuantidade(int *numTabs) {
+  FILE *arqTab = fopen("qtdtabelas.txt", "r");
+  if (arqTab == NULL) {
+    printf("Arquivo não encontrado!");
+  } else {
+    fscanf(arqTab, "%i", numTabs);
+  }
+}
 
 int main() {
 
@@ -178,6 +385,14 @@ int main() {
   // Cria um vetor que guardará todas as tabelas;
   Tabela *tabelas = NULL;
 
+  carregarQuantidade(&numTab);
+  printf("num tabs: %d\n", numTab);
+
+  if (numTab > 0) {
+    numTab = 0;
+    tabelas = carregarTabelas(tabelas, &numTab);
+  }
+
   do {
     printf("Digite a opção desejada: ");
     scanf("%d", &opcao);
@@ -188,8 +403,12 @@ int main() {
       break;
     case 1:
       tabelas = inserirTabela(tabelas, &numTab, criarTabela());
+      salvarTabela(tabelas[numTab - 1]);
+      salvarNome(tabelas, numTab);
+      salvarQuantidade(numTab);
       break;
     case 2:
+      printf("%d", numTab);
       listarTabelas(tabelas, numTab);
       break;
     case 3:
@@ -207,44 +426,27 @@ int main() {
       if (tabelaExiste == false) {
         printf("TABELA NÃO ENCONTRADA\n");
       } else {
-        printf("TABELA ENCONTRADA - %d\n", idTab);
-
         adicionarLinha(&tabelas[idTab]);
+        salvarTabela(tabelas[idTab]);
       }
-    }
+      break;
+    case 4:
+      printf("Digite o nome da tabela: ");
+      scanf("%s", nomeTabela);
 
+      for (idTab = 0; idTab < numTab; idTab++) {
+        if (strcmp(tabelas[idTab].nomeTabela, nomeTabela) == 0) {
+          tabelaExiste = true;
+          break;
+        }
+      }
+
+      if (tabelaExiste == false) {
+        printf("TABELA NÃO ENCONTRADA\n");
+      } else {
+        listarDadosTabela(tabelas[idTab]);
+      }
+      break;
+    }
   } while (opcao != 0);
-
-  printf("%d\n", tabelas[0].dados[0][0].dataInt);
-  printf("%s\n", tabelas[0].dados[0][1].dataString);
-
-  // Tabela tabela_teste = criarTabela();
-
-  // printf("Nome: \n%s", tabela_teste.nomeTabela);
-
-  // adicionarLinha(&tabela_teste);
-  // printf("Linhas: \n%d", tabela_teste.numLinhas);
-
-  /*
-  ----- TUDO AQUI É TESTE NADA DEFINITO, mas tá td funcionado
-  */
-  /*printf("Nome da Tabela: %s\n", tabela.nomeTabela);
-  printf("Colunas e Linhas: %d %d\n", tabela.numColunas, tabela.numLinhas);
-
-  for (int i = 0; i < tabela.numColunas; i++) {
-    printf("Nome da %dª Coluna: %s\n", (i + 1), tabela.colunas[i].nomeColuna);
-    printf("Tipo da %dª Coluna: %d\n", (i + 1), tabela.colunas[i].tipoColuna);
-  }
-
-  printf("Nome da ChavePrimária: %s\n", tabela.chavePrimaria.nomeColuna);
-  printf("Tipo da ChavePrimária: %d\n", tabela.chavePrimaria.tipoColuna);
-
-  printf("\nTABELA\n");
-
-  for (int i = 0; i < tabela.numLinhas; i++) {
-    for (int j = 0; j < tabela.numColunas; j++) {
-      printf(" %d ", tabela.dados[i][j].dataInt);
-    }
-    printf("\n");
-  }*/
 }
